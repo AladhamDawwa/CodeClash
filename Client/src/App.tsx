eacf22725949575d11f2dded55a071b70645478f
Ahmed Adel <62815241+ahmedadel2020-code@users.noreply.github.com>
2024-04-16 07:58:05 +0200

    modify FE folder structure

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,45 +1,44 @@
-import './App.css'
-import {createBrowserRouter, RouterProvider, redirect} from "react-router-dom";
-import SignUp from "./components/SignUp";
-// import _404 from "./components/404";
-// import SignIn from './components/Auth/SignIn';
-// import HomePage from "./components/HomePage";
-import Landing from "./components/Landing";
-import SignIn from './components/Auth/SignIn/SignIn';
-import _404 from './components/404/404';
+import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+} from "react-router-dom";
+import SignUp from "./components/Auth/SignUp";
+import Landing from "./pages/Landing";
+import SignIn from "./components/Auth/SignIn/SignIn";
+import _404 from "./components/404/404";
 
 let isUserAuthenticated = false;
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Landing/>,
-    loader : () => {
+    element: <Landing />,
+    loader: () => {
       return isUserAuthenticated ? redirect("/home") : null;
     },
   },
-    // {
-    //   path: "/home",
-    //   element: <HomePage/>,
-    // },
+  // {
+  //   path: "/home",
+  //   element: <HomePage/>,
+  // },
   {
     path: "/signIn",
-    element: <SignIn/>,
+    element: <SignIn />,
   },
   {
     path: "/signUp",
-    element: <SignUp/>,
+    element: <SignUp />,
   },
   {
     path: "*",
-    element: <_404/>,
-  }
+    element: <_404 />,
+  },
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- Client/src/components/Auth/index.ts
+++
@@ -1,2 +0,0 @@
-import _ from "./Auth";
-export default _;
