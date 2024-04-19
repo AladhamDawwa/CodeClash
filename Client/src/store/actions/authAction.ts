ebb9b8ae75214f79139e005a2877f8a540b6b96d
Ahmed Adel <62815241+ahmedadel2020-code@users.noreply.github.com>
2024-04-19 10:09:57 +0200

    fix the navigation when user email is  wrong

--- Client/src/store/actions/authAction.ts
+++ Client/src/store/actions/authAction.ts
@@ -17,7 +17,7 @@ export const signInAction = createAsyncThunk(
         },
       );
 
-      return response.data.user; // Replace with the actual data structure
+      return response.data.user;
     } catch (error) {
       const err = error as AxiosError;
       const errorData = err.response?.data as ResponseData;
