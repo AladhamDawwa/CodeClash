7ff9b67fcc5745ee8c8581440fb4819e20533e73
ShehabGad5 <skhaled@instabug.com>
2024-06-20 17:52:19 +0300

    description field in user

--- backend/src/game/store/i_game_store.ts
+++
@@ -1,8 +0,0 @@
-import { GameDetails, OneVOneGameDetails } from "../game_details/game_details";
-
-export interface IGameStore {
-  create(game_details: GameDetails): string
-  delete(game_id: string): void
-  update(game_details: GameDetails): GameDetails
-  get(game_id: string): GameDetails
-}
