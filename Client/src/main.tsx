950c9cf104f4269679603443ee1e85661206d9ff
ahmedthecreator <111194945+ahmedthecreator@users.noreply.github.com>
2024-06-21 23:36:01 +0300

    updated App, handling deletion of local storage

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -2,18 +2,18 @@ import { ThemeProvider } from '@emotion/react';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App.tsx';
 import './index.css';
 import { persistor, store } from './store/store.ts';
 import theme from './styles/theme.ts';
 import { PersistGate } from 'redux-persist/integration/react';
+import AppWrapper from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <StrictMode>
         <ThemeProvider theme={theme}>
-          <App />
+          <AppWrapper />
         </ThemeProvider>
       </StrictMode>
     </PersistGate>
