3e4c8eb10316d492455eeb867f459040f24b09c0
AladhamDawwa <aladhamdawwa@gmail.com>
2024-07-11 12:59:26 +0300

    removed pathfinder-ui

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,8 +15,6 @@ import { LMSGameSocketController } from './socket_controllers/lms_game'
 import { TvTGameSocketController } from './socket_controllers/tvt_game'
 import { UsersUnsolvedProblems } from './models/users_unsolved_problems'
 import { NotificationsController } from './controllers/notifications'
-import { Users } from './models/users'
-import { Admins } from './models/admin'
 import { AdminController } from './controllers/admin'
 
 dotenv.config()
@@ -26,12 +24,6 @@ const port = process.env.PORT || 8080
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
-
-const pathfinderUI = require('pathfinder-ui')
-app.use('/pathfinder', function (req, res, next) {
-  pathfinderUI(app)
-  next()
-}, pathfinderUI.router)
 
 // routes
 UsersController.routes(app)
